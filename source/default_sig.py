256ad452816e890247ad78b52141cccc1e851a6a
Mikiyasu Nishiyama <mickey.1n13m4@gmail.com>
2019-05-28 08:32:13 +0900

    added default_sig

---
+++ source/default_sig.py
@@ -0,0 +1,19 @@
+import wave
+from pydub import *
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_name="../sample_voice/fast1.wav"
+#音声ファイルの読み込み
+sound = AudioSegment.from_file(file_name,"wav")
+
+#listに変換する
+samples=sound.get_array_of_samples()
+
+#実際に用いる部分の信号のみ表示
+plt.xlim([130000,200000])
+
+#凡例の追加を行う
+plt.plot(samples)
+plt.show()
