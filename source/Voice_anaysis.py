1bd48cf48878e010f3f4264cfadbf88d88d5acca
Mikiyasu Nishiyama <mickey.1n13m4@gmail.com>
2019-06-03 06:15:35 +0900

    default_sig fixed

--- source/Voice_anaysis.py
+++ source/Voice_anaysis.py
@@ -1,29 +1,49 @@
 import wave
+import os
 from pydub import *
 import scipy.fftpack
 import numpy as np
 import matplotlib.pyplot as plt
+
+path="../sample_voice/"
 # 音声ファイルの読み込み
-file_name="../sample_voice/slow1.wav"
-sound = AudioSegment.from_file(file_name,"wav")
+print(os.listdir(path))
+direct=os.listdir(path)
+#sound = AudioSegment.from_file(file_name,"wav")
 
-# 音声データをリストで抽出
-list_sound = sound.get_array_of_samples()
+for x in direct:
+    if os.path.isdir(path+x):
+        file_name=path+x
+        sound=AudioSegment.from_file(file_name,"wav")
 
-#numpyにステレオ音源の片側の音源を抽出しておく
-sample=np.array(sound.get_array_of_samples())
-#正規化
-x=np.frombuffer(sample,dtype="int16")/32768.0
+        # 音声データをリストで抽出
+        list_sound = sound.get_array_of_samples()
 
-#点を取る位置、数を設定
-start=0
-N=256
-#窓関数(hammingwindowをかける)
-HammingWindow=np.hamming(N)
-x=HammingWindow*sample[start:N+start]
+        #numpyにステレオ音源の片側の音源を抽出しておく
+        sample=np.array(sound.get_array_of_samples())
 
-spec=np.fft.fft(x)
+        #元々の入力信号を左側に表示
+        plt.xlabel("Time")
+        plt.ylabel("Signal")
+        plt.plot(sample)
 
-#信号をグラフにプロットする
-plt.plot(spec)
-plt.show()
+        #正規化
+        x=np.frombuffer(sample,dtype="int16")/32768.0
+
+        #点を取る位置、数を設定
+        start=0
+        N=256
+
+        #窓関数(hammingwindowをかける)
+        HammingWindow=np.hamming(N)
+        x=HammingWindow*sample[start:N+start]
+
+        spec=np.fft.fft(x)
+        Amp=np.abs(spec)
+
+        #信号をグラフにプロットする(右側に表示)
+        #plt.subplot(122)
+        plt.xlabel("Frequency")
+        plt.ylabel("Amplitude")
+        plt.plot(Amp)
+        plt.show()
--- source/default_sig.py
+++ source/default_sig.py
@@ -1,19 +1,31 @@
 import wave
 from pydub import *
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_name="../voice_trim/fast1.wav"
-#音声ファイルの読み込み
-sound = AudioSegment.from_file(file_name,"wav")
+path="../sample_voice/"
+# 音声ファイルの読み込み
+print(os.listdir(path))
+direct=os.listdir(path)
 
-#listに変換する
-samples=sound.get_array_of_samples()
+for x in direct:
+    if os.path.exists(path+x):
+        file_name=path+x
+        sound=AudioSegment.from_file(file_name,"wav")
 
-#実際に用いる部分の信号のみ表示
-#plt.xlim([130000,200000])
+        # 音声データをリストで抽出
+        list_sound = sound.get_array_of_samples()
 
-#凡例の追加を行う
-plt.plot(samples)
+        #numpyにステレオ音源の片側の音源を抽出しておく
+        sample=np.array(sound.get_array_of_samples())
+
+        #実際に用いる部分の信号のみ表示
+        plt.xlim([130000,200000])
+
+        #凡例の追加を行う
+        plt.plot(sample)
 plt.show()
+
+        
