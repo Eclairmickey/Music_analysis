81cc315da27f433568bbce5e76dd9622b5c0e7e8
Mikiyasu Nishiyama <mickey.1n13m4@gmail.com>
2019-06-04 04:51:41 +0900

    added powerspect.py and fixed

--- source/default_sig.py
+++ source/default_sig.py
@@ -5,23 +5,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path="../sample_voice/"
+path="../trim_voice/"
 # 音声ファイルの読み込み
 print(os.listdir(path))
 direct=os.listdir(path)
 
 i=0
 for x in direct:
-    if os.path.exists(path+x):
+    if os.path.exists(path+x) and i<6:
         #path結合して音源の読み込み
         file_name=path+x
         sound=AudioSegment.from_file(file_name,"wav")
-
+        
+        print(sound.channels)
         # 音声データをリストで抽出
-        list_sound = sound.get_array_of_samples()
-
-        #numpyにステレオ音源の片側の音源を抽出しておく
-        sample=np.array(sound.get_array_of_samples())
+        sample = sound.get_array_of_samples()
 
         #実際に用いる部分の信号のみ表示
         #plt.xlim([130000,200000])
--- source/powerspect.py
+++ source/powerspect.py
@@ -3,40 +3,52 @@ import os
 from pydub import *
 import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 start=0
 N=512
 
 #パスの指定
-path="../voice_trim/"
+path="../trim_voice/"
 direct=os.listdir(path)
 
 i=0
 for x in direct:
     if os.path.exists(path+x):
         #path結合して音源の読み込み
-        
         file_name=path+x
         sound=AudioSegment.from_file(file_name,"wav")
-
-        #音声データをリストで抽出 
+        print(sound.frame_rate)
+        # 音声データをリストで抽出 
         sound_ar = sound.get_array_of_samples()
 
         #numpyにステレオ音源の片側の音源を抽出しておく
         sample =sound_ar[::sound.channels]
 
+        #正規化
+        x=np.frombuffer(sample,dtype="int16")/32768.0
+
         #窓関数(hammingwindowをかける)
         HammingWindow=np.hamming(N)
         x=HammingWindow*sample[start:N+start]
 
-        #フーリエ変換を行う
+        #FFT
         spec=np.fft.fft(x)
 
+        #パワースペクトル
+        fft_signal_power=np.sqrt(abs(spec))
+
+        #分析の長さで割る
+        fft_signal_power=fft_signal_power/N
+
+        #デシベル換算
+        fft_signal_power=10*np.log10(fft_signal_power)
+
+        
         num=i%3
         plt.subplot(1,3,num+1)
         print(i,',',num)
         #信号をグラフにプロットする
-        plt.plot(spec)
+        plt.plot(fft_signal_power)
         i+=1
 
 plt.show()
