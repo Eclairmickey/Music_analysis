0dc15e716bfa17051889f797253f3080f41a444e
N.mickey <mickey.1n13m4@gmail.com>
2019-08-29 01:30:34 +0900

    クロマグラム表示

--- source/chroma_gram.py
+++ source/chroma_gram.py
@@ -12,11 +12,10 @@ sr=44100
 #音源読み込み(audio.load(ファイル名,offset,duration))
 #offset部分からdurationで設定した秒数分切り出し
 file_name="Sample_Song/AtlanticoBlue.mp3"
-music,fs=librosa.audio.load(file_name,sr=sr)
-
-
+music,fs=librosa.audio.load(file_name,sr=sr,offset=1.0,duration=15.0)
+print(music)
 #打楽器成分とメロディ成分に分ける
-D_harmonic,D_percussive=librosa.decompose.hpss(D)
+D_harmonic,D_percussive=librosa.effects.hpss(music)
 
 #クロマグラムを構成
 """
@@ -29,6 +28,15 @@ D_harmonic,D_percussive=librosa.decompose.hpss(D)
 C=librosa.feature.chroma_cens(y=D_harmonic,sr=sr)
 
 #プロット部分
+
+# プロットします
+plt.figure(figsize=(12,4))
+librosa.display.specshow(C, sr=sr, x_axis='time', y_axis='chroma', vmin=0, vmax=1)
+plt.title('Chromagram')
+plt.colorbar()
+plt.tight_layout()
+plt.show()
+
 
 
 
